aac0eca4a956fa934db1916011241ad0c9dde9d5
chanhthi1502 <nguyenchanhthi92@gmail.com>
2021-11-30 14:34:59 +0700

    Commit new data test

--- src/dataSource/pomelo_data_test.js
+++ src/dataSource/pomelo_data_test.js
@@ -1,8 +1,8 @@
 module.exports = {
    test:{
        data:{
-            "Mali Blouse - Rose in Blouses Category" : 
-                     {category: 'Blouses', product: 'Mali Blouse - Rose', size : 'M', quantity :'3', newSize :'L', promoCode: 'AAABC'},
+            "Dirty Top - Rosewood" : 
+                     {category: 'Blouses', product: 'Dirty Top - Rosewood', size : 'S', quantity :'3', newSize :'M', promoCode: 'AAABC'},
             "Polka Dot Midi Dress - Pink in Dresses Category" : 
                      {category: 'Dresses', product: 'Polka Dot Midi Dress - Pink', size : 'M', quantity :'3', newSize :'L', promoCode: 'AAABC'}
        }
