f47d1f1da71681d46b3aa6e3bc2abc1290e1223e
chanhthi1502 <nguyenchanhthi92@gmail.com>
2021-12-07 10:18:09 +0700

    fix test run 1 data & runner bug

--- src/dataSource/pomelo_data_test.js
+++ src/dataSource/pomelo_data_test.js
@@ -1,8 +1,6 @@
 module.exports = {
    test:{
        data:{
-            "Dirty Top - Rosewood" : 
-                     {category: 'Blouses', product: 'Dirty Top - Rosewood', size : 'S', quantity :'3', newSize :'M', promoCode: 'AAABC'},
             "Polka Dot Midi Dress - Pink in Dresses Category" : 
                      {category: 'Dresses', product: 'Polka Dot Midi Dress - Pink', size : 'M', quantity :'3', newSize :'L', promoCode: 'AAABC'}
        }
